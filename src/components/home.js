b3774d91eef45c5b39d9b4cc34d15e238badfb13
Sudhanshu Gulhane <sudhanshu072000@gmail.com>
2021-02-15 09:50:53 +0530

    creating home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+//home page for 3 buttons login register and signup
+
 function Home() {
     return (
             <div className="home-card">
